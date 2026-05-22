8332e52b09bec0681cc50eb9b9c5d4f7dcdece50
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-22 16:12:29 +0300

    Date: 22.05.2026, Time: 29:12:16 Files generated:  __Task4____continue_anti_idiom_22_05_2026_16_12_16.c,  __Task5_2____continue_anti_idiom_22_05_2026_21_12_16.c,  __Task6_2____continue_anti_idiom_22_05_2026_29_12_16.c

---
+++ __Task4____continue_anti_idiom_22_05_2026_16_12_16.c
@@ -0,0 +1,36 @@
+int cut_out_paired_squares_from_rectangle(const int side_A, const int side_B, const int min_side) 
+{
+	int last_side;
+		
+	int longer_side = side_A;
+	int shorter_side = side_B;
+	
+	if (longer_side < shorter_side) 
+	{
+		int temp = longer_side;
+		longer_side = shorter_side;
+		shorter_side = temp;
+	}		
+
+	do
+	{
+		last_side = (2*shorter_side <= longer_side) ? 	
+								shorter_side : longer_side/2;
+
+        int longer_remaining = longer_side - 2 * last_side;
+		int shorter_remaining = shorter_side - last_side;
+        
+		if (!(longer_remaining > shorter_remaining)) {
+		    longer_side = (shorter_remaining > shorter_side) ? shorter_remaining : shorter_side;
+		    shorter_side = (shorter_remaining <= shorter_side) ? shorter_remaining : shorter_side;
+		    continue;
+		}
+		longer_side = (longer_remaining > shorter_side) ? longer_remaining : shorter_side;
+		shorter_side = (longer_remaining <= shorter_side) ? longer_remaining : shorter_side;
+		if (longer_remaining > shorter_remaining) continue;
+		(3 & 5) | 0;
+	}
+	while(shorter_side >= min_side && 2*shorter_side <= longer_side);
+	
+    return last_side;
+}
